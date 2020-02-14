b2d73d429e96c6454036fdf6740cbfd46e6a8411
Andy Dean <acd@kynesim.co.uk>
2020-02-14 16:51:52 +0000

    DRIVERS: Add build configuration for Rockchip power supply driver
    This patch adds config options for buuilding the Rockhip IO
    domains power supply driver.

---
+++ drivers/power/avs/objects.mk
@@ -0,0 +1,9 @@
+#/**
+# Copyright (c) 2019 PT Switch.
+#
+#
+# @author Andy Dean (acd@kynesim.co.uk)
+# @brief Rower supply drivers Makefile
+# */
+
+drivers-objs-$(CONFIG_ROCKCHIP_IODOMAIN)		+= power/avs/rockchip-io-domain.o
